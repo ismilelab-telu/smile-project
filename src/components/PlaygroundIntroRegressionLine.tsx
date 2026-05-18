f1370641710f867902fc640177eb07fc404a609f
fxrdhan <firdausarief65@gmail.com>
2026-05-19 01:32:50 +0700

    fix(playground): adjust intro regression line

--- src/components/PlaygroundIntroRegressionLine.tsx
+++ src/components/PlaygroundIntroRegressionLine.tsx
@@ -4,12 +4,12 @@ export function PlaygroundIntroRegressionLine(props: SVGProps<SVGSVGElement>) {
   return (
     <svg fill="none" viewBox="0 0 170 95" xmlns="http://www.w3.org/2000/svg" {...props}>
       <path
-        d="M-2 104 L156 50"
+        d="M-5 95 L175 7"
         data-playground-regression-line
         stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
-        strokeWidth="4"
+        strokeWidth="7"
         vectorEffect="non-scaling-stroke"
       />
     </svg>
